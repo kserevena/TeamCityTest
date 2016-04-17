eff038085d9446419da7c271efa14b85c52a0b61
Karl Serevena <kserevena@mylifedigital.co.uk>
2016-04-17 12:36:29 +0100

    Added a deliberatly failing test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * Created by karlserevena on 17/04/2016.
@@ -14,4 +15,9 @@ public class HelloWorldTest {
         assertThat(helloWorld.sayHello(), is("hello"));
     }
 
+    @Test
+    public void failingTest() throws Exception {
+        fail();
+
+    }
 }
